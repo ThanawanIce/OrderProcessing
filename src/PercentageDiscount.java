f8ce6d4818502be08db21572337cafebb4bc1814
ThanawanIce <thanawan.phus@ku.th>
2026-08-27 16:57:49 +0700

    Add files via upload

--- src/PercentageDiscount.java
+++ src/PercentageDiscount.java
@@ -12,6 +12,8 @@ public class PercentageDiscount implements DiscountStrategy {
         double total = order.getTotalPrice();
         // TODO (2a): คืนราคาหลังหักส่วนลด percent%
         //   hint: total - (total * percent / 100.0)
+        total = total - (total * percent / 100.0);
+        
         return /* ====== replace this ====== */ total;
     }
 }
